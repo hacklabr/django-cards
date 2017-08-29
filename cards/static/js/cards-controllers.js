c0ef35f01ae2b314ad6f9663966396d3d214cc42
leopiccionia <leopiccionia@gmail.com>
2017-08-29 19:52:00 -0300

    Assim Se Faz's SPA initial commit.

--- cards/static/js/cards-controllers.js
+++ cards/static/js/cards-controllers.js
@@ -2,9 +2,67 @@
     'use strict';
     var app = angular.module('cards.controllers', ['ngCookies']);
 
-    app.controller('CardsCtrl', [
-        '$scope',
-        function ($scope) {
+    app.controller('CardsListCtrl', ['$scope', '$routeParams', '$http', 'Audiences', 'Axes', 'Cards', 'Likes', 'Tags', 'YouTubeEmbeds',
+        function ($scope, $routeParams, $http, Audiences, Axes, Cards, Likes, Tags, YouTubeEmbeds) {
+            /* Services bindings */
+            $scope.audiences = Audiences.query();
+            $scope.cards = Cards.query();
+            $scope.axes = Axes.query();
+            $scope.tags = Tags.query();
+
+            $scope.show_filter_options = false;
+            $scope.keyword = '';
+            $scope.selected_audience = '';
+            $scope.selected_axis = '';
+            $scope.selected_status = '';
+            $scope.selected_tags = [];
+
+            $scope.last_searched = '';
+
+            function filter_by_status () {
+                $scope.certified_cards = $scope.cards.slice(0).
+                    filter(function (elem) {
+                        return elem.is_certified;
+                    });
+                $scope.unattached_cards = $scope.cards.slice(0).
+                    filter(function (elem) {
+                        return !elem.is_certified;
+                    });
+            }
+
+            $scope.filtered_query = function () {             
+                Cards.query({
+                    audience__name: $scope.selected_audience,
+                    axis__name: $scope.selected_axis,
+                    is_certified: $scope.selected_status,
+                    search: $scope.keyword,
+                    tags__name: $scope.selected_tags
+                }).$promise.then(function (data) {
+                    console.log(data);
+                    $scope.cards = data;
+                    filter_by_status();
+                });
+            }
+
+            $scope.search_keyword = function() {
+                $scope.last_searched = $scope.keyword;
+                $scope.cards = Cards.query({search: $scope.keyword});
+                $scope.cards.$promise.then(filter_by_status);
+            }
+
+            $scope.cards.$promise.then(filter_by_status);          
+        }
+    ]);
+
+    app.controller('CardDetailCtrl', ['$scope', '$routeParams', '$http', 'Cards', 'Likes', 'YouTubeEmbeds',
+        function ($scope, $routeParams, $http, Cards, Likes, YouTubeEmbeds) {
+            $scope.card_id = $routeParams.cardId; 
+            $scope.card = Cards.get({id: $scope.card_id});
+        }
+    ]); 
+
+    app.controller('NewCardCtrl', ['$scope', '$routeParams', '$http', 'Cards', 'Likes', 'YouTubeEmbeds',
+        function ($scope, $routeParams, $http, Cards, Likes, YouTubeEmbeds) {
 
         }
     ]);
--- cards/static/js/cards-services.js
+++ cards/static/js/cards-services.js
@@ -2,11 +2,34 @@
     'use strict';
     var app = angular.module('cards.services', ['ngResource']);
 
-    // app.factory('Forum', ['$resource', function($resource){
-    //     return $resource('/discussion/api/forum/:id',
-    //         {'id' : '@id'},
-    //         {'update': {'method': 'PUT'} });
-    // }]);
+    app.factory('Audiences', ['$resource', function($resource){
+        return $resource('/cards/api/audience/:id',
+            {'id': '@id'});
+    }]);
 
+    app.factory('Axes', ['$resource', function($resource){
+        return $resource('/cards/api/axis/:id',
+            {'id': '@id'});
+    }]);    
+
+    app.factory('Cards', ['$resource', function($resource){
+        return $resource('/cards/api/cards/:id',
+             {'id': '@id'});
+    }]);
+
+    app.factory('Likes', ['$resource', function($resource){
+        return $resource('/cards/api/likes/:id',
+            {'id': '@id'});
+    }]);
+
+    app.factory('Tags', ['$resource', function($resource){
+        return $resource('/cards/api/tags/:id',
+            {'id': '@id'});
+    }]);
+
+    app.factory('YouTubeEmbeds', ['$resource', function($resource){
+        return $resource('/cards/api/youtube_embeds/:id',
+            {'id': '@id'});
+    }]);
 
 })(window.angular);
