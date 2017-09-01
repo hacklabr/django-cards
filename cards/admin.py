4e25d1aa0b84405fcab2a1b98ae3ca8b130d0b1f
Tiago <tiagofassoni@gmail.com>
2017-09-01 16:47:54 -0300

    Authors in admin interface

--- cards/admin.py
+++ cards/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Audience, Axis, Card, Image, Like, YoutubeEmbed
+from .models import Audience, Authors, Axis, Card, Image, Like, YoutubeEmbed
 # from tagulous.admin import register as tagulous_register
+
+class AuthorsInline(admin.TabularInline):
+    model = Authors
 
 class ImageInline(admin.TabularInline):
     model = Image
@@ -10,10 +13,12 @@ class YoutubeEmbedInline(admin.TabularInline):
 
 class CardAdmin(admin.ModelAdmin):
     inlines = [
+        AuthorsInline,
         ImageInline,
         YoutubeEmbedInline
     ]
 
+admin.site.register(Authors)
 admin.site.register(Audience)
 admin.site.register(Axis)
 admin.site.register(Card, CardAdmin)
