0bcfa79fcf1e4587ca790e2d5ade084a9550d008
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2020-08-12 15:06:07 -0300

    Feat: update card-edit component

--- cards/static/js/card-edit/card-edit.component.js
+++ cards/static/js/card-edit/card-edit.component.js
@@ -1,10 +1,9 @@
 (function() {
     'use strict';
     angular.
-        module('cardEdit').
-        component('cardEdit', {
+        module('cardEdit').component('cardEdit', {
             templateUrl: '/cards/card-edit.template.html',
-            controller: 'CardDetailCtrl',
+            controller: CardEditController,
             bindings: {
                 card: '<',
             },
@@ -12,13 +11,24 @@
 
         CardEditController.$inject = [
         '$scope',
-        'Audiences',
-        'Axes',
+        '$stateParams',
         'Cards',
-        'Tags',
     ];
 
-    function CardEditController ($scope, Audiences, Axes, Cards, Tags) {
-
+    function CardEditController ($scope, $stateParams, Cards) {
+        ctrl.$onInit = function() {
+            $scope.card_id = $stateParams.cardId;
+            $scope.classroom_id = $stateParams.classroomId;
+            $scope.card = Cards.get({id: $scope.card_id}, function(response) {
+               $scope.card = response;
+               var image_slides = $scope.card.image_gallery.map(function(img) {
+                   return {type: 'image', el: img};
+               });
+               var video_slides = $scope.card.youtube_embeds.map(function(video) {
+                   return {type: 'video', el: video};
+               });
+               $scope.slides = image_slides.concat(video_slides);
+           });
+       };
     }
 })();
