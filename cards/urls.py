a1fae3d0c0c6b9236f40ec840f9975c753e074c0
Eduardo Nunes <eduardonunes2525@gmail.com>
2021-09-02 10:57:46 -0300

    Creating theme and format pagination

--- cards/urls.py
+++ cards/urls.py
@@ -8,8 +8,10 @@ from . import views
 router = routers.SimpleRouter(trailing_slash=False)
 
 router.register(r'audience', views.AudienceViewSet, base_name='audience')
+router.register(r'audiences', views.AudiencesPageViewSet, base_name='audiences')
 # router.register(r'authors', views.AuthorsViewSet, base_name='authors')
 router.register(r'axis', views.AxisViewSet, base_name='axis')
+router.register(r'axes', views.AxesPageViewSet, base_name='axes')
 router.register(r'cards', views.CardViewSet, base_name='cards')
 router.register(r'tags', views.TagsViewSet, base_name='tags')
 router.register(r'images', views.ImageViewSet, base_name='image')
